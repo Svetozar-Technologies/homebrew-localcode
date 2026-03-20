4cb81555abc5be0cacbdd39d03afea156ccb51c5
Kaivalya <kaivalyaanand.pandey@prepladder.com>
2026-03-20 18:06:02 +0530

    Fix SHA256 checksums for v0.4.4 release

--- Formula/localcode.rb
+++ Formula/localcode.rb
@@ -7,22 +7,22 @@ class Localcode < Formula
   on_macos do
     on_arm do
       url "https://github.com/Svetozar-Technologies/LocalCode/releases/download/v#{version}/localcode-darwin-arm64.tar.gz"
-      sha256 "8fee6dad9f57f63a59130d880fe2e5e897aef10b426972d242b0429be551b8af"
+      sha256 "863828ee953920692c3affe0abbed978a4cb31e84907a4f13b39359683cf4ee1"
     end
     on_intel do
       url "https://github.com/Svetozar-Technologies/LocalCode/releases/download/v#{version}/localcode-darwin-x64.tar.gz"
-      sha256 "8e7c1d9b317e245712e5b56152dc79d7086039af5811347860fd8987c67be7ae"
+      sha256 "5a1f41bff486f06d91d89d2164d8407ca53a4700f60f99e38534defe36f04994"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Svetozar-Technologies/LocalCode/releases/download/v#{version}/localcode-linux-arm64.tar.gz"
-      sha256 "cbd00a611d167e522803e7589df2f23edad9bade7521d74b8720c00bbd30a35b"
+      sha256 "6ac6b3ba39dc4753919fe4431d10a07607676311f39837ef67c99d5ae8b83273"
     end
     on_intel do
       url "https://github.com/Svetozar-Technologies/LocalCode/releases/download/v#{version}/localcode-linux-x64.tar.gz"
-      sha256 "f7656f27776c2a3c05834c50971284d0ec0cf2b9da04c46c70815aa93375465b"
+      sha256 "c698f5660c83dd7199331b901a5cf3c10aa200e52d5270a443454bec2099fdda"
     end
   end
 
